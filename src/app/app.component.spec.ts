b6253d9fa93c7b04215f4b7a73fa0584af0632e4
Task Manager Developer <developer@taskmanager.com>
2025-10-01 08:47:58 -0500

    Removed redundant appconfig files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { WeatherService } from './services/weather.service';
+import { StockService } from './services/stock.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [
+        provideHttpClient(),
+        WeatherService,
+        StockService
+      ]
     }).compileComponents();
   });
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,7 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-
+import { ApplicationConfig } from '@angular/core';
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
   ]
 };
