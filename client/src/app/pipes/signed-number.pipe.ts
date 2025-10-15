692d7272f3a242a0c051e3cf20aea61b9c708276
Task Manager Developer <developer@taskmanager.com>
2025-10-15 14:56:09 -0500

    format change on signed number pipe

--- client/src/app/pipes/signed-number.pipe.ts
+++ client/src/app/pipes/signed-number.pipe.ts
@@ -1,12 +1,12 @@
 // signed-number.pipe.ts
 import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({
-  name: 'signedNumber',
+    name: 'signedNumber',
 })
 export class SignedNumberPipe implements PipeTransform {
-  transform(value: number | string | null): string {
-    if (value === null) {return '';}
-    const result = typeof value === 'number' ? value : parseFloat(value);
-    return result >= 0 ? `+${result}` : result.toString();
-  }
+    transform(value: number | string | null): string {
+        if (value === null) { return ''; }
+        const result = typeof value === 'number' ? value : parseFloat(value);
+        return result >= 0 ? `+${result}` : result.toString();
+    }
 }
